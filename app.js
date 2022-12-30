b7a0cb18f14264d2607bf5ad27c25aa72c0b25ac
Romina Natalia Martínez <105136327+mart1020@users.noreply.github.com>
2022-12-29 21:48:20 -0300

    nuevo avance

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 
 
+let pagina = 1;
+
+const getData = async () => {
+    const url = `https://www.digi-api.com/api/v1/digimon?page=0${pagina}`
+    const response = await fetch(url)
+    const json = await response.json();
+    printData(json)
+}
+
+getData()
+
+const printData = arr => {
+    console.log(arr)
+}
+
