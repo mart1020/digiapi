7215a91c24e05d2161ac843adec80f0d32acd038
Romina Natalia Martínez <105136327+mart1020@users.noreply.github.com>
2022-12-30 23:46:09 -0300

    version final

--- app.js
+++ app.js
@@ -1,37 +1,64 @@
 
 
 const root = document.getElementById('root');
+const loader = document.getElementById('contenedor');
 
+
+//------------------------------------------
+//filtro
+//------------------------------------------
+
+const todos = document.getElementById('todos') 
+const Training = document.getElementById('Training') 
+const Rookie = document.getElementById('Rookie') 
+const Champion = document.getElementById('Champion') 
+const Fresh = document.getElementById('Fresh') 
+const Mega = document.getElementById('Mega') 
+const Ultimate = document.getElementById('Ultimate') 
+const Armor = document.getElementById('Armor') 
+
+
+
+
+
+
+//----------------------------------------
 let pagina = 1;
 
 const getData = async () => {
-    const url = `https://api.disneyapi.dev/characters?page=${pagina}`
+    const url = `https://digimon-api.vercel.app/api/digimon?page=${pagina}`
     const response = await fetch(url)
     const json = await response.json();
-    printData(json.data)
-}
+    printData(json)
+    console.log(json)
+    setTimeout(() => {
+       root.classList.remove('esconder')
+       loader.classList.add('esconder')
+       },1200)
+       data = json;
+    }
+    
+    //let data = {};
 
 getData()
+
+//----------------------------------------
+//tarjetas
+//----------------------------------------
 
 const printData = arr =>{
     let card = ``;
     arr.forEach((personaje)=> {
         card = card + `
-            <div class="col s12 m6 l3">
+            <div class="col s12 m6 l2">
             <div class="card">
             <div class="card-image">
-                <img src="https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png">
+                <img src=${personaje.img}>
 
                 </div>
                 <div class="card-content">
-                    <p>Aliados</p>
-                    <p>Enemigos</p>
-                    <p>Pelicula</p>
-                    <p>Parques de atracciones</p>
-                    <p>Cortos</p>
-                    <p>Programas TV</p>
-                    <p>Videojuegos</p>
-                    <p>Id</p>
+                    <p>Nombre: ${personaje.name}<p>
+                    <p>nivel: ${personaje.level}</p>
                     
                 </div>
             <div class="card-action">
@@ -45,4 +72,54 @@ const printData = arr =>{
 
 }
 
+//-----------------------------------------------
+// filtros
+//-------------------------------------------
+Training.addEventListener('click',() => {
+    const arr = data;
+    const arrTraining = arr.filter(personaje => personaje.level == 'In Training')
+    printData(arrTraining)
+})
+
+Rookie.addEventListener('click',() => {
+    const arr = data;
+    const arrRookie = arr.filter(personaje => personaje.level == 'Rookie')
+    printData(arrRookie)
+})
+
+Champion.addEventListener('click',() => {
+    const arr = data;
+    const arrChampion = arr.filter(personaje => personaje.level == 'Champion')
+    printData(arrChampion)
+})
+
+
+Fresh.addEventListener('click',() => {
+    const arr = data;
+    const arrFresh = arr.filter(personaje => personaje.level == 'Fresh')
+    printData(arrFresh)
+})
+
+Mega.addEventListener('click',() => {
+    const arr = data;
+    const arrMega = arr.filter(personaje => personaje.level == 'Mega')
+    printData(arrMega)
+})
+
+Ultimate.addEventListener('click',() => {
+    const arr = data;
+    const arrUltimate = arr.filter(personaje => personaje.level == 'Ultimate')
+    printData(arrUltimate)
+})
+
+Armor.addEventListener('click',() => {
+    const arr = data;
+    const arrArmor = arr.filter(personaje => personaje.level == 'Armor')
+    printData(arrArmor)
+})
+
+todos.addEventListener('click',() => {
+    const arr = data;
+    printData(arr)
+})
 
